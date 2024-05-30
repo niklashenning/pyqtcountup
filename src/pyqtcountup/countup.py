4b410e2e53e9600393b2d979c0bdcd6d08b49578
Niklas Henning <niklashenningbusiness@gmail.com>
2024-05-30 13:26:22 +0200

    Add remaining methods and refactor code

--- src/pyqtcountup/countup.py
+++ src/pyqtcountup/countup.py
@@ -7,93 +7,152 @@ class CountUp(QObject):
 
     finished = Signal()
 
-    def __init__(self, label: QLabel, from_value: int | float = 0, to_value: int | float = 100,
-                 duration: int = 1000, decimals: int = 0, decimal_point: str = '.',
-                 thousands_separator: str = '', prefix: str = '',
-                 prefix_before_minus: bool = True, suffix: str = '',
-                 easing: QEasingCurve = QEasingCurve.Type.OutCubic):
+    def __init__(self, label: QLabel, start_value: int | float = 0, end_value: int | float = 100,
+                 duration: int = 1000, decimal_places: int = 0, decimal: str = '.',
+                 thousands_separator: str = '', prefix: str = '', prefix_before_minus: bool = True,
+                 suffix: str = '', easing: QEasingCurve = QEasingCurve.Type.OutCubic):
         super(CountUp, self).__init__(None)
 
         self.label = label
-        self.from_value = from_value
-        self.to_value = to_value
+        self.start_value = start_value
+        self.end_value = end_value
         self.duration = duration
-        self.decimals = decimals
-        self.decimal_point = decimal_point
+        self.decimal_places = decimal_places
+        self.decimal = decimal
         self.thousands_separator = thousands_separator
         self.prefix = prefix
         self.prefix_before_minus = prefix_before_minus
         self.suffix = suffix
         self.easing = easing
 
+        self.value = 0
+        self.is_running = False
+        self.timeline = QTimeLine(self.duration, self.label)
+        self.timeline.setEasingCurve(easing)
+
     def start(self):
-        def frame_changed(timeline_value):
-            value = Utils.get_value_from_timeline_value(timeline_value, self.decimals)
-            value_string = Utils.format_value(value, self.decimals,
-                                              self.decimal_point, self.thousands_separator)
+        frame_range_start = Utils.get_timeline_value_from_value(self.start_value, self.decimal_places)
+        frame_range_end = Utils.get_timeline_value_from_value(self.end_value, self.decimal_places)
 
-            if not self.prefix_before_minus and value < 0:
-                value_string = value_string[1:]
-                full_string = "-" + self.prefix + value_string + self.suffix
-            else:
-                full_string = self.prefix + value_string + self.suffix
+        self.timeline.setFrameRange(frame_range_start, frame_range_end)
+        self.timeline.frameChanged.connect(lambda v: self.__frame_changed(v))
+        self.timeline.finished.connect(self.__timeline_finished)
+        self.__frame_changed(frame_range_start)
+        self.is_running = True
+        self.timeline.start()
 
-            self.label.setText(full_string)
-
-        def timeline_finished():
-            self.finished.emit()
-
-        frame_range_start = Utils.get_timeline_value_from_value(self.from_value, self.decimals)
-        frame_range_end = Utils.get_timeline_value_from_value(self.to_value, self.decimals)
-
-        timeline = QTimeLine(self.duration, self.label)
-        timeline.setFrameRange(frame_range_start, frame_range_end)
-        timeline.setEasingCurve(QEasingCurve.Type.OutCubic)
-        timeline.frameChanged.connect(frame_changed)
-        timeline.finished.connect(timeline_finished)
-        timeline.start()
+    def update(self, new_end_value: int):
+        self.stop()
+        self.setStartValue(self.value)
+        self.setEndValue(new_end_value)
+        self.start()
 
     def pause(self):
-        pass
+        self.timeline.setPaused(True)
+        self.is_running = False
 
     def resume(self):
-        pass
+        self.is_running = True
+        self.timeline.resume()
 
     def stop(self):
-        pass
+        self.timeline.stop()
+        self.is_running = False
 
     def reset(self):
-        pass
+        self.timeline.stop()
+        self.is_running = False
+        self.__frame_changed(Utils.get_timeline_value_from_value(self.start_value, self.decimal_places))
+
+    def getLabel(self) -> QLabel:
+        return self.label
 
     def setLabel(self, label: QLabel):
         self.label = label
 
-    def setFromValue(self, from_value: int | float):
-        self.from_value = from_value
+    def getStartValue(self) -> int | float:
+        return self.start_value
 
-    def setToValue(self, to_value: int | float):
-        self.to_value = to_value
+    def setStartValue(self, start_value: int | float):
+        self.start_value = start_value
+
+    def getEndValue(self) -> int | float:
+        return self.end_value
+
+    def setEndValue(self, end_value: int | float):
+        self.end_value = end_value
+
+    def setStartEndValues(self, start_value: int | float, end_value: int | float):
+        self.start_value = start_value
+        self.end_value = end_value
+
+    def getDuration(self) -> int:
+        return self.duration
 
     def setDuration(self, duration: int):
         self.duration = duration
+        self.timeline.setDuration(duration)
 
-    def setDecimals(self, decimals: int):
-        self.decimals = decimals
+    def getDecimalPlaces(self) -> int:
+        return self.decimal_places
 
-    def setDecimalPoint(self, decimal_point: str):
-        self.decimal_point = decimal_point
+    def setDecimalPlaces(self, decimal_places: int):
+        self.decimal_places = decimal_places
+
+    def getDecimal(self) -> str:
+        return self.decimal
+
+    def setDecimal(self, decimal: str):
+        self.decimal = decimal
+
+    def getThousandsSeparator(self) -> str:
+        return self.thousands_separator
 
     def setThousandsSeparator(self, thousands_separator: str):
         self.thousands_separator = thousands_separator
 
+    def getPrefix(self) -> str:
+        return self.prefix
+
     def setPrefix(self, prefix: str):
         self.prefix = prefix
+
+    def isPrefixBeforeMinus(self) -> bool:
+        return self.prefix_before_minus
 
     def setPrefixBeforeMinus(self, enabled: bool):
         self.prefix_before_minus = enabled
 
+    def getSuffix(self) -> str:
+        return self.suffix
+
     def setSuffix(self, suffix: str):
         self.suffix = suffix
 
+    def getEasing(self) -> QEasingCurve:
+        return self.easing
+
     def setEasing(self, easing: QEasingCurve):
         self.easing = easing
+        self.timeline.setEasingCurve(easing)
+
+    def isRunning(self) -> bool:
+        return self.is_running
+
+    def __frame_changed(self, timeline_value: int):
+        value = Utils.get_value_from_timeline_value(timeline_value, self.decimal_places)
+        self.value = value
+        value_string = Utils.format_value(value, self.decimal_places,
+                                          self.decimal, self.thousands_separator)
+
+        if not self.prefix_before_minus and value < 0:
+            value_string = value_string[1:]
+            full_string = "-" + self.prefix + value_string + self.suffix
+        else:
+            full_string = self.prefix + value_string + self.suffix
+
+        self.label.setText(full_string)
+
+    def __timeline_finished(self):
+        self.is_running = False
+        self.finished.emit()
--- src/pyqtcountup/utils.py
+++ src/pyqtcountup/utils.py
@@ -1,9 +1,10 @@
 
+
 class Utils:
 
     @staticmethod
-    def format_value(value, decimals, decimal_point, thousands_separator):
-        string_format = '{:,.' + str(decimals) + 'f}'
+    def format_value(value, decimal_places, decimal, thousands_separator):
+        string_format = '{:,.' + str(decimal_places) + 'f}'
         temp_thousands_separator = '0888019ca0faa9774a728c864e248749'
         temp_decimal_separator = 'f6e9eccf7256112eccd52f41f1fded3f'
 
@@ -11,18 +12,18 @@ class Utils:
                              .replace(',', temp_thousands_separator)
                              .replace('.', temp_decimal_separator)
                              .replace(temp_thousands_separator, thousands_separator)
-                             .replace(temp_decimal_separator, decimal_point))
+                             .replace(temp_decimal_separator, decimal))
 
     @staticmethod
-    def get_timeline_value_from_value(timeline_value, decimals):
-        if decimals <= 0:
+    def get_timeline_value_from_value(timeline_value, decimal_places):
+        if decimal_places <= 0:
             return int(timeline_value)
 
-        return int(timeline_value * 10 ** decimals)
+        return int(timeline_value * 10 ** decimal_places)
 
     @staticmethod
-    def get_value_from_timeline_value(value, decimals):
-        if decimals <= 0:
+    def get_value_from_timeline_value(value, decimal_places):
+        if decimal_places <= 0:
             return value
 
-        return value / 10 ** decimals
+        return value / 10 ** decimal_places
