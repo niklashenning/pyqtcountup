9d295d5c5cd6fd442d760e17c87cf000151d1f35
Niklas Henning <niklashenningbusiness@gmail.com>
2024-06-01 20:32:32 +0200

    Add documentation and comments

--- src/pyqtcountup/countup.py
+++ src/pyqtcountup/countup.py
@@ -5,14 +5,31 @@ from .utils import Utils
 
 class CountUp(QObject):
 
+    # Signal
     finished = Signal()
 
     def __init__(self, label: QLabel, start_value: int | float = 0, end_value: int | float = 100,
                  duration: int = 1000, decimal_places: int = 0, decimal: str = '.',
                  thousands_separator: str = '', prefix: str = '', prefix_before_minus: bool = True,
                  suffix: str = '', easing: QEasingCurve.Type | None = QEasingCurve.Type.OutCubic):
+        """Create a new CountUp instance
+
+        :param label: label to animate the text of
+        :param start_value: start value of the animation
+        :param end_value: end value of the animation
+        :param duration: duration of the animation
+        :param decimal_places: amount of decimal places that will be displayed
+        :param decimal: decimal of the number
+        :param thousands_separator: thousands separator of the number
+        :param prefix: prefix that will be shown before the value
+        :param prefix_before_minus: whether to show the prefix before or after the minus for negative values
+        :param suffix: suffix that will be shown behind the value
+        :param easing: easing curve of the animation
+        """
+
         super(CountUp, self).__init__(None)
 
+        # Init attributes
         self.__label = label
         self.__start_value = start_value
         self.__end_value = end_value
@@ -27,6 +44,8 @@ class CountUp(QObject):
 
         self.__value = 0
         self.__is_running = False
+
+        # Init timeline
         self.__timeline = QTimeLine(self.__duration, self.__label)
         self.__timeline.setDuration(duration)
 
@@ -39,6 +58,8 @@ class CountUp(QObject):
         self.__timeline.finished.connect(self.__timeline_finished)
 
     def start(self):
+        """Start the animation"""
+
         frame_range_start = Utils.get_timeline_value_from_value(self.__start_value, self.__decimal_places)
         frame_range_end = Utils.get_timeline_value_from_value(self.__end_value, self.__decimal_places)
         self.__timeline.setFrameRange(frame_range_start, frame_range_end)
@@ -47,120 +68,263 @@ class CountUp(QObject):
         self.__timeline.start()
 
     def update(self, new_end_value: int):
+        """Update the animation end value while the animation is running"""
+
         self.__timeline.stop()
         self.setStartValue(self.__value)
         self.setEndValue(new_end_value)
         self.start()
 
     def pause(self):
+        """Pause the running animation"""
+
         self.__timeline.setPaused(True)
         self.__is_running = False
 
     def resume(self):
+        """Resume the paused animation"""
+
         self.__is_running = True
         self.__timeline.resume()
 
     def stop(self):
+        """Stop the animation"""
+
         self.__timeline.stop()
         self.__is_running = False
 
     def reset(self):
+        """Reset the animation and show the start value"""
+
         self.__timeline.stop()
         self.__is_running = False
         self.__frame_changed(Utils.get_timeline_value_from_value(self.__start_value, self.__decimal_places))
 
     def getLabel(self) -> QLabel:
+        """Get the label
+
+        :return: label
+        """
+
         return self.__label
 
     def setLabel(self, label: QLabel):
+        """Set the label to animate the text of
+
+        :param label: new label
+        """
+
         self.__label = label
 
     def getStartValue(self) -> int | float:
+        """Get the start value of the animation
+
+        :return: start value
+        """
+
         return self.__start_value
 
     def setStartValue(self, start_value: int | float):
+        """Set the start value of the animation
+
+        :param start_value: new start value
+        """
+
         self.__start_value = start_value
 
     def getEndValue(self) -> int | float:
+        """Get the end value of the animation
+
+        :return: end value
+        """
+
         return self.__end_value
 
     def setEndValue(self, end_value: int | float):
+        """Set the end value of the animation
+
+        :param end_value: new end value
+        """
+
         self.__end_value = end_value
 
     def setStartEndValues(self, start_value: int | float, end_value: int | float):
+        """Set the start and end values of the animation
+
+        :param start_value: new start value
+        :param end_value: new end value
+        """
+
         self.__start_value = start_value
         self.__end_value = end_value
 
     def getDuration(self) -> int:
+        """Get the duration of the animation
+
+        :return: duration
+        """
+
         return self.__duration
 
     def setDuration(self, duration: int):
+        """Set the duration of the animation
+
+        :param duration: new duration
+        """
+
         self.__duration = duration
         self.__timeline.setDuration(duration)
 
     def getDecimalPlaces(self) -> int:
+        """Get the amount of decimal places of the number
+
+        :return: amount of decimal places
+        """
+
         return self.__decimal_places
 
     def setDecimalPlaces(self, decimal_places: int):
+        """Set the amount of decimal places of the number
+
+        :param decimal_places: new amount of decimal places
+        """
+
         self.__decimal_places = decimal_places
 
     def getDecimal(self) -> str:
+        """Get the decimal of the number
+
+        :return: decimal
+        """
+
         return self.__decimal
 
     def setDecimal(self, decimal: str):
+        """Set the decimal of the number
+
+        :param decimal: new decimal
+        """
+
         self.__decimal = decimal
 
     def getThousandsSeparator(self) -> str:
+        """Get the thousands separator of the number
+
+        :return: thousands separator
+        """
+
         return self.__thousands_separator
 
     def setThousandsSeparator(self, thousands_separator: str):
+        """Set the thousands separator of the number
+
+        :param thousands_separator: new thousands separator
+        """
+
         self.__thousands_separator = thousands_separator
 
     def getPrefix(self) -> str:
+        """Get the prefix that will be shown before the number
+
+        :return: prefix
+        """
+
         return self.__prefix
 
     def setPrefix(self, prefix: str):
+        """Set the prefix that will be shown before the number
+
+        :param prefix: new prefix
+        """
+
         self.__prefix = prefix
 
     def isPrefixBeforeMinus(self) -> bool:
+        """Get whether the prefix is shown before or after the minus for negative values
+
+        :return: whether the prefix is shown before the minus for negative values
+        """
+
         return self.__prefix_before_minus
 
     def setPrefixBeforeMinus(self, enabled: bool):
+        """Set whether the prefix is shown before or after the minus for negative values
+
+        :param enabled: whether the prefix should be shown before the minus for negative values
+        """
+
         self.__prefix_before_minus = enabled
 
     def getSuffix(self) -> str:
+        """Get the suffix that will be shown after the number
+
+        :return: suffix
+        """
+
         return self.__suffix
 
     def setSuffix(self, suffix: str):
+        """Set the suffix that will be shown after the number
+
+        :param suffix: new suffix
+        """
+
         self.__suffix = suffix
 
     def getEasing(self) -> QEasingCurve.Type | None:
+        """Get the easing curve of the animation
+
+        :return: easing curve
+        """
+
         return self.__easing
 
     def setEasing(self, easing: QEasingCurve.Type | None):
+        """Set the easing curve of the animation
+
+        :param easing: new easing curve
+        """
+
         self.__easing = easing
+
         if easing is None:
             self.__timeline.setEasingCurve(QEasingCurve.Type.Linear)
         else:
             self.__timeline.setEasingCurve(easing)
 
     def isRunning(self) -> bool:
+        """Get whether the animation is currently running
+
+        :return: whether the animation is running
+        """
+
         return self.__is_running
 
     def __frame_changed(self, timeline_value: int):
+        """React to the frameChanged signal of the QTimeLine
+        and updates the label with the new value
+
+        :param timeline_value: the current value of the timeline
+        """
+
+        # Convert timeline value to real value and format
         value = Utils.get_value_from_timeline_value(timeline_value, self.__decimal_places)
         self.__value = value
         value_string = Utils.format_value(value, self.__decimal_places,
                                           self.__decimal, self.__thousands_separator)
 
+        # Add prefix and suffix
         if not self.__prefix_before_minus and value < 0:
             value_string = value_string[1:]
             full_string = "-" + self.__prefix + value_string + self.__suffix
         else:
             full_string = self.__prefix + value_string + self.__suffix
 
+        # Set label text
         self.__label.setText(full_string)
 
     def __timeline_finished(self):
+        """Handle finished signal of QTimeLine and emit own finished signal"""
+
         self.__is_running = False
         self.finished.emit()
--- tests/utils_test.py
+++ tests/utils_test.py
@@ -2,6 +2,9 @@ from src.pyqtcountup.utils import Utils
 
 
 def test_format_value():
+    """Test the format_value method used to convert a number
+    into a string with given formatting options"""
+
     assert Utils.format_value(1201.24, 3, '.', ',') == '1,201.240'
     assert Utils.format_value(-14212.88, 2, ',', ' ') == '-14 212,88'
     assert Utils.format_value(7846.4231, 2, '.', '') == '7846.42'
@@ -9,10 +12,16 @@ def test_format_value():
 
 
 def test_get_timeline_value_from_value():
+    """Test the get_timeline_value_from_value method used to
+    convert a value into the correct timeline value"""
+
     assert Utils.get_timeline_value_from_value(124.78, 2) == 12478
     assert Utils.get_timeline_value_from_value(-2832.63, 2) == -283263
 
 
 def test_get_value_from_timeline_value():
+    """Test the get_value_from_timeline_value method used to
+    convert a timeline value into the correct value"""
+
     assert Utils.get_value_from_timeline_value(12478, 2) == 124.78
     assert Utils.get_value_from_timeline_value(-283263, 2) == -2832.63
